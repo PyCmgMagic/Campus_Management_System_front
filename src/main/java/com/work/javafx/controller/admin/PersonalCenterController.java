60d448e4785449eacebeb836b828e8dda017cda8
x1x-1 <liao_changxiao630@163.com>
2025-05-06 22:12:56 +0800

    管理员个人中心

---
+++ src/main/java/com/work/javafx/controller/admin/PersonalCenterController.java
@@ -0,0 +1,8 @@
+package com.work.javafx.controller.admin;
+
+import javafx.event.ActionEvent;
+
+public class PersonalCenterController {
+    public void PersonalCenterController(ActionEvent event) {
+    }
+}
